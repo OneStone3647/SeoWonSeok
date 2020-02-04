db7521fcc2d4fdd3bf84d222e89d2ab607edd8a5
OneStone3647 <mister.karun@gmail.com>
2020-02-04 15:07:45 +0900

    20.02.04 AnimImage Update

---
+++ 과제/AnimImage/AnimImage/WinMain.cpp
@@ -0,0 +1,92 @@
+#include "Player.h"
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("AnimImage");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 1024, 768,
+		NULL, (HMENU)NULL, hInstance, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0))
+	{
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	HDC MemDC;
+	HBITMAP Image, OldBitmap;
+	int bX, bY;
+	BITMAP Bit;
+
+
+	switch (iMessage)
+	{
+	case WM_CREATE:
+		Player::GetInstance()->Init(hWnd, wParam);
+		return 0;
+	case WM_KEYDOWN:
+		Player::GetInstance()->Input();
+		InvalidateRect(hWnd, NULL, TRUE);
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		MemDC = CreateCompatibleDC(hdc);
+
+		// 직접적으로 이미지를 로드한다.
+		Image = (HBITMAP)LoadImage(NULL, TEXT("image.bmp"),
+			IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+		// Image 선택 & 기존의 비트맵 저장
+		OldBitmap = (HBITMAP)SelectObject(MemDC, Image);
+
+		// 이미지의 정보를 가져온다.
+		GetObject(Image, sizeof(BITMAP), &Bit);
+		bX = Bit.bmWidth;
+		bY = Bit.bmHeight;
+
+		//투명처리와 부분그리기
+		//이번에는 왼쪽을 보고 있고 한발을 내딛은 캐릭터를 그린다
+		TransparentBlt(hdc, Player::GetInstance()->GetPlayerX(), Player::GetInstance()->GetPlayerY(), bX / 4, bY / 4,
+			MemDC, (bX / 4) * Player::GetInstance()->GetFrame(), (bY / 4) * Player::GetInstance()->GetPalyerPosition(), bX / 4, bY / 4, RGB(255, 0, 255));
+
+		SelectObject(MemDC, OldBitmap);
+		DeleteObject(Image);
+		DeleteDC(MemDC);
+
+		EndPaint(hWnd, &ps);
+		return 0;
+
+	case WM_DESTROY:
+		Player::GetInstance()->Release();
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
