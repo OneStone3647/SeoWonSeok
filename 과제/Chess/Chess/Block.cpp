bcbfa48d86672d22248dbfcc959e9fda01bcbb52
OneStone3647 <mister.karun@gmail.com>
2020-02-06 12:10:10 +0900

    20.02.06 Chess Update

--- 과제/Chess/Chess/Block.cpp
+++ 과제/Chess/Chess/Block.cpp
@@ -6,29 +6,6 @@ Block::Block()
 {
 }
 
-void Block::Init(HDC hdc, HINSTANCE hInst, BLOCKTYPE blockType, PIECETYPE pieceType)
-{
-	m_X = 0;
-	m_Y = 0;
-	m_BlockType = BLOCKTYPE_NONE;
-	m_PieceType = PIECETYPE_NONE;
-
-	if (block->GetBlockType() == BLOCKTYPE_FIELD01)
-	{
-		NewBitmap = (HBITMAP)LoadImage(NULL, TEXT("block00.bmp"),
-			IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
-	}
-	else if (block->GetBlockType() == BLOCKTYPE_FIELD02)
-	{
-		NewBitmap = (HBITMAP)LoadImage(NULL, TEXT("block01.bmp"),
-			IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
-	}
-}
-
-void Block::Draw(HDC hdc, int x, int y)
-{
-}
-
 
 Block::~Block()
 {
--- 과제/Chess/Chess/Block.h
+++ 과제/Chess/Chess/Block.h
@@ -1,62 +1,8 @@
 #pragma once
-#include <Windows.h>
-
-enum BLOCKTYPE
-{
-	BLOCKTYPE_FIELD01 = 1,
-	BLOCKTYPE_FIELD02,
-	BLOCKTYPE_SELECT,
-	BLOCKTYPE_BLACK,
-	BLOCKTYPE_WITHE
-};
-
-// Ã¼½º¸» enum
-enum PIECETYPE
-{
-	PIECETYPE_NONE = 0,
-	PIECETYPE_KING,
-	PIECETYPE_QUEEN,
-	PIECETYPE_BISHOP,
-	PIECETYPE_KNIGHT,
-	PIECETYPE_ROOK,
-	PIECETYPE_PAWN
-};
-
 class Block
 {
-private:
-	HDC			MemDC;
-	int			m_X;
-	int			m_Y;
-	BLOCKTYPE	m_BlockType;
-	PIECETYPE	m_PieceType;
-	HBITMAP		m_NewBlock;
-	HBITMAP		m_OldBlock;
-
 public:
 	Block();
-	
-	void Init(HDC hdc, HINSTANCE hInst, BLOCKTYPE blockType, PIECETYPE pieceType = PIECETYPE_NONE);
-	void Draw(HDC hdc, int x, int y);
-
-	void SetBlockType(BLOCKTYPE BlockType)
-	{
-		m_BlockType = BlockType;
-	}
-	BLOCKTYPE GetBlockType()
-	{
-		return m_BlockType;
-	}
-
-	void SetPieceType(PIECETYPE PieceType)
-	{
-		m_PieceType = PieceType;
-	}
-	PIECETYPE GetPieceType()
-	{
-		return m_PieceType;
-	}
-
 	~Block();
 };
 
