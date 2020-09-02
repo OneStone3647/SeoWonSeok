69cda25fe8f60bca30f2901ecc9cfd65e98f3f8d
OneStone3647 <mister.karun@gmail.com>
2020-09-03 01:43:39 +0900

    20.09.03 Update
    - MatchGame Ver 0.1

--- 과제/CSharp/MatchGame/MatchGame/MatchGame.Designer.cs
+++ 과제/CSharp/MatchGame/MatchGame/MatchGame.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label20 = new System.Windows.Forms.Label();
             this.label19 = new System.Windows.Forms.Label();
@@ -49,6 +50,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -99,6 +101,7 @@
             this.label20.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label20.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label20.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label20.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label20.Location = new System.Drawing.Point(581, 704);
             this.label20.Name = "label20";
             this.label20.Size = new System.Drawing.Size(139, 234);
@@ -111,6 +114,7 @@
             this.label19.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label19.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label19.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label19.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label19.Location = new System.Drawing.Point(437, 704);
             this.label19.Name = "label19";
             this.label19.Size = new System.Drawing.Size(136, 234);
@@ -123,6 +127,7 @@
             this.label18.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label18.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label18.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label18.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label18.Location = new System.Drawing.Point(293, 704);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(136, 234);
@@ -135,6 +140,7 @@
             this.label17.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label17.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label17.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label17.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label17.Location = new System.Drawing.Point(149, 704);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(136, 234);
@@ -147,6 +153,7 @@
             this.label16.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label16.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label16.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label16.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label16.Location = new System.Drawing.Point(5, 704);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(136, 234);
@@ -159,6 +166,7 @@
             this.label15.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label15.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label15.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label15.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label15.Location = new System.Drawing.Point(581, 470);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(139, 232);
@@ -171,6 +179,7 @@
             this.label14.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label14.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label14.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label14.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label14.Location = new System.Drawing.Point(437, 470);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(136, 232);
@@ -183,6 +192,7 @@
             this.label13.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label13.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label13.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label13.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label13.Location = new System.Drawing.Point(293, 470);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(136, 232);
@@ -195,6 +205,7 @@
             this.label12.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label12.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label12.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label12.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label12.Location = new System.Drawing.Point(149, 470);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(136, 232);
@@ -207,6 +218,7 @@
             this.label11.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label11.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label11.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label11.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label11.Location = new System.Drawing.Point(5, 470);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(136, 232);
@@ -219,6 +231,7 @@
             this.label10.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label10.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label10.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label10.Location = new System.Drawing.Point(581, 236);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(139, 232);
@@ -231,6 +244,7 @@
             this.label9.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label9.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label9.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label9.Location = new System.Drawing.Point(437, 236);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(136, 232);
@@ -243,6 +257,7 @@
             this.label8.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label8.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label8.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label8.Location = new System.Drawing.Point(293, 236);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(136, 232);
@@ -255,6 +270,7 @@
             this.label7.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label7.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label7.Location = new System.Drawing.Point(149, 236);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(136, 232);
@@ -267,6 +283,7 @@
             this.label6.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label6.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label6.Location = new System.Drawing.Point(5, 236);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(136, 232);
@@ -279,6 +296,7 @@
             this.label5.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label5.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label5.Location = new System.Drawing.Point(581, 2);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(139, 232);
@@ -291,6 +309,7 @@
             this.label4.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label4.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label4.Location = new System.Drawing.Point(437, 2);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(136, 232);
@@ -303,6 +322,7 @@
             this.label3.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label3.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label3.Location = new System.Drawing.Point(293, 2);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(136, 232);
@@ -315,6 +335,7 @@
             this.label2.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label2.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label2.Location = new System.Drawing.Point(149, 2);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(136, 232);
@@ -327,12 +348,18 @@
             this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label1.Location = new System.Drawing.Point(5, 2);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(136, 232);
             this.label1.TabIndex = 0;
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 750;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // MatchGame
             // 
@@ -370,6 +397,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- 과제/CSharp/MatchGame/MatchGame/MatchGame.cs
+++ 과제/CSharp/MatchGame/MatchGame/MatchGame.cs
@@ -67,17 +67,78 @@ namespace MatchGame
 
         private void Label_Click(object sender, EventArgs e)
         {
+            // 첫번째 클릭과 두번째 클릭을 했을 경우 일종의 쿨타임
+            if(firstClicked != null && secondClicked != null)
+            {
+                return;
+            }
+
             // as 연산자: 형변환이 가능하면 형변환을 수행하고, 그렇지 않으면 null값을 대입합니다.
             Label clickedLabel = sender as Label;
             if(clickedLabel != null)
             {
+                if(clickedLabel.ForeColor == null)
+                {
+                    return;
+                }
+
                 if(clickedLabel.ForeColor == Color.Black)
                 {
                     return;
                 }
 
-                clickedLabel.ForeColor = Color.Black;
+                // 첫번째 클릭이 없다면 첫번째 클릭을 등록합니다.
+                if(firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+                    return;
+                }
+
+                // 두번째 클릭을 등록합니다.
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                CheckForWinner();
+
+                // 첫번째 클릭돠 두번째 클릭이 같을 경우
+                if(firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                }
+                else
+                {
+                    timer1.Start();
+                }
             }
+        }
+
+        private void CheckForWinner()
+        {
+            Label label;
+            for(int i =0; i<tableLayoutPanel1.Controls.Count; i++)
+            {
+                label = tableLayoutPanel1.Controls[i] as Label;
+                if (label != null && label.ForeColor == label.BackColor)
+                {
+                    return;
+                }
+            }
+
+            MessageBox.Show("You Win!!");
+            Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
         }
     }
 }
